561322b3d41c03e2eea17e1a98c05f06b6e22244
rifatKasikci <78976774+rifatKasikci@users.noreply.github.com>
2021-02-14 13:31:42 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<Tkey,Tvalue>
+    {
+
+
+        Tkey[] Tkey_Array;
+        Tvalue[] Tvalue_Array;
+
+
+        Tkey[] Tkey_tempArray;
+        Tvalue[] Tvalue_tempArray;
+
+        public MyDictionary()
+        {
+           Tkey_Array = new Tkey[0];
+            Tvalue_Array = new Tvalue[0];
+        }
+
+        public void Add(Tkey tkey , Tvalue tvalue)
+        {
+            Tkey_tempArray = Tkey_Array;
+            Tvalue_tempArray = Tvalue_Array;
+
+            Tkey_Array = new Tkey[Tkey_Array.Length + 1];
+            Tvalue_Array = new Tvalue[Tvalue_Array.Length + 1];
+
+            for (int i = 0; i < Tkey_tempArray.Length; i++)
+            {
+                Tkey_Array[i] = Tkey_tempArray[i];
+                
+            }
+
+            for (int i = 0; i < Tvalue_tempArray.Length; i++)
+            {
+                Tvalue_Array[i] = Tvalue_tempArray[i];
+            }
+
+            Tkey_Array[Tkey_Array.Length - 1] = tkey;
+            Tvalue_Array[Tvalue_Array.Length - 1] = tvalue;
+
+        }
+
+        public int Count
+        {
+
+            get { return Tkey_Array.Length; }
+
+        }
+
+       
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> isimler = new Dictionary<int, string>();
+            isimler.Add(1,"Helin");
+            isimler.Add(2, "Selin");
+            isimler.Add(3, "Gizem");
+            isimler.Add(4, "Alp");
+            isimler.Add(5, "Bekir");
+            Console.WriteLine("Dictionary : " + isimler.Count);
+            Console.WriteLine("****************************************");
+            MyDictionary<int, string> isimler2 = new MyDictionary<int, string>();
+            isimler2.Add(1,"Ali");
+            isimler2.Add(2, "Mehmet");
+            isimler2.Add(3, "Ayşe");
+            isimler2.Add(4, "Fatma");
+            isimler2.Add(5, "Huriye");
+            isimler2.Add(6, "Salih");
+            Console.WriteLine("MyDictionary : " + isimler2.Count);
+
+
+            /*ÇIKTI:
+            
+          Dictionary : 5
+          ****************************************
+          MyDictionary : 6
+          
+            */
+        }
+    }
+}
